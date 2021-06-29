5185dd4d5363a8cae770482dcaae1175e61277cc
jfaulk3 <faulknerjimmie@gmail.com>
2021-06-29 12:39:47 -0500

    removed dev package

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -4,7 +4,6 @@ require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 const express = require("express");
 const cors = require("cors");
-const morgan = require("morgan");
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
@@ -12,7 +11,6 @@ const reservationsRouter = require("./reservations/reservations.router");
 const tablesRouter = require("./tables/tables.router");
 const app = express();
 
-app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
